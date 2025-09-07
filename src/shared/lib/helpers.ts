b74d6e56bd79d1e10f6063c3afa322936b1072c9
eldyarcodes <thedolbekov2@gmail.com>
2025-09-07 15:41:39 +0600

    refactor: helpers

--- src/shared/lib/helpers.ts
+++ src/shared/lib/helpers.ts
@@ -2,22 +2,19 @@ import blitzMode from '@/shared/assets/images/blitz-mode.svg';
 import bulletMode from '@/shared/assets/images/bullet-mode.svg';
 import rapidMode from '@/shared/assets/images/rapid-mode.svg';
 
-export function formatTime(seconds: number) {
+export function formatTime(seconds: number): string {
   const h = String(Math.floor(seconds / 3600)).padStart(2, '0');
   const m = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
   const s = String(seconds % 60).padStart(2, '0');
   return `${h}:${m}:${s}`;
 }
 
-export function getTimeModeImage(mode: number) {
-  switch (mode) {
-    case 15:
-      return bulletMode;
-    case 30:
-      return blitzMode;
-    case 60:
-      return rapidMode;
-    default:
-      return '';
-  }
+export function getTimeModeImage(mode: number): string {
+  return (
+    {
+      15: bulletMode,
+      30: blitzMode,
+      60: rapidMode,
+    }[mode] ?? ''
+  );
 }
