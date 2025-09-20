9e8fbbc4f75670b7d61bc1b60d7028d33b83c6bc
Eldyar <thedolbekov2@gmail.com>
2025-09-20 22:51:47 +0300

    feat: tailwind configs

---
+++ tailwind.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      screens: {
+        'max-sm': { raw: '(max-width: 639px)' },
+        'max-md': { raw: '(max-width: 767px)' },
+        'max-lg': { raw: '(max-width: 1023px)' },
+        'max-xl': { raw: '(max-width: 1279px)' },
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
